107894be9c8dc76a25ec9556c3aa6fd66e101e97
PuceVinicius <vinicius.moreno.ismart@gmail.com>
2023-03-20 12:53:40 -0300

    Added ReadOnly and Foldout editor attributes

---
+++ Assets/Editor/ReadOnly/Editor/ReadOnlyEditor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property,
+        GUIContent label) =>
+        EditorGUI.GetPropertyHeight(property, label, true);
+
+    public override void OnGUI(Rect position,
+        SerializedProperty property,
+        GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
